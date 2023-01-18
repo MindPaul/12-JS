c5fa80b0db5326cb9e8e9a1e5a701a67164a17ee
Mindaugas <mindaugas.paulius0@gmail.com>
2023-01-18 23:13:11 +0200

    nd liko 5-6

--- ND/002-functionn.js
+++ ND/002-functionn.js
@@ -97,3 +97,64 @@ console.log(' -------Funkcija pavadinimu “didziausiasSkaiciusSarase”:-------
 // rezultatas: “Pateikta netinkamo tipo reikšmė.”
 // console.log( didziausiasSkaiciusSarase( [] ) );
 // rezultatas: “Pateiktas sąrašas negali būti tuščias.”
+
+function didziausiasSkaiciusSarase (a) {
+  let max = 0;
+  for (let i = 0; i < a.length; i++) {
+    const nr = a[i];
+    if (nr > max) {
+      max = nr;
+    }
+  }
+  if (!Array.isArray(a)) {
+    return '“Pateikta netinkamo tipo reikšmė.”';
+  }
+  if (a.length === 0) {
+    return '“Pateiktas sąrašas negali būti tuščias.”'
+  }
+  return max;
+    //return Math.max(a);
+  }
+
+console.log( didziausiasSkaiciusSarase( [  ] ) );
+console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
+console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
+console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
+console.log( didziausiasSkaiciusSarase( [] ) );
+console.log( didziausiasSkaiciusSarase( "pomidoras" ) );
+
+
+console.log(' -------isrinktiRaides”:-----------');
+// Funkcija pavadinimu “isrinktiRaides”:
+// priima du kintamuosius:
+// pirmasis nurodo tekstą, su kuriuo reikės atlikti užduotį
+// antrasis nurodo kas kelintą raidę išrinkti
+// patikrinti, ar pirmasis kintamasis yra teksto tipo:
+// jei ne, išvedame pranešimą “Pirmasis kintamasis yra netinkamo tipo.”
+// priešingu atveju tęsiame darbą
+// patikrinti, ar pirmasis kintamasis yra ne tuščias tekstas ir ne didesnis nei 100 simbolių:
+// jei ne, išvedame pranešimą “Pirmojo kintamojo reikšmė yra netinkamo dydžio.”
+// priešingu atveju tęsiame darbą
+// patikrinti, ar antrasis kintamasis yra skaičiaus tipo:
+// jei ne, išvedame pranešimą “Antrasis kintamasis yra netinkamo tipo.”
+// priešingu atveju tęsiame darbą
+// patikriname, ar antrojo kintamojo vertė yra didesnė už nulį:
+// jei ne, išvedame pranešimą “Antrasis kintamasis turi būti didesnis už nulį.”
+// priešingu atveju tęsiame darbą
+// patikriname, ar antrojo kintamojo vertė yra ne didesnė už pirmojo kintamojo ilgį:
+// jei ne, išvedame pranešimą “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+// priešingu atveju tęsiame darbą
+// išrenkame iš nurodyto teksto kas kelintą raidę (pagal antrojo kintamojo žingsnį)
+// išrinktas raides sudėti į atskirą kintamąjį, kuris yra teksto tipo
+// gražina rezultatą
+// TESTAI:
+// console.log( isrinktiRaides( “abcdefg”, 2 ) );
+// rezultatas: “bdf”
+// console.log( isrinktiRaides( “abcdefghijkl”, 3 ) );
+// rezultatas: “cfil”
+// console.log( isrinktiRaides( “abc”, 0 ) );
+// rezultatas: “Antrasis kintamasis turi būti didesnis už nulį.”
+// console.log( isrinktiRaides( “abc”, 4 ) );
+// rezultatas: “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+// console.log( isrinktiRaides( 1561, 2 ) );
+// rezultatas: “Pirmasis kintamasis yra netinkamo tipo.”
